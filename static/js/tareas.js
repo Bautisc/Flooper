d495440bfb12e23040e2490cd0efba2b78b8a89a
Bautista Sanguinetti <bautiscsc.123@gmail.com>
2021-11-12 23:04:13 -0300

    nuevo tareas.js

---
+++ static/js/tareas.js
@@ -0,0 +1,38 @@
+// Selectores
+var input_Tareas = document.querySelector(".input-tareas");
+var tareas_Btn = document.querySelector(".tareas-btn");
+var lista_Tareas = document.querySelector(".lista-tareas");
+
+//Eventos
+tareas_Btn.addEventListener('click', AgregarTarea);
+
+//Funciones
+function AgregarTarea(event){
+        //Esto evita que se recargue la página cada vez que se agrega una tarea.
+    event.preventDefault();
+        //Generar div que contiene las tareas
+    const div_Tareas = document.createElement("div");
+    div_Tareas.classList.add("tareas");
+        //Esto genera un li que contiene la tarea
+    const nueva_Tarea = document.createElement('li');
+    nueva_Tarea.innerText = input_Tareas.value;
+    nueva_Tarea.classList.add('item-tareas');
+    div_Tareas.appendChild(nueva_Tarea);
+
+    //Esto genera los botones de completado y eliminar dentro del div
+
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("completar-btn");
+    div_Tareas.appendChild(completedButton)
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteButton.classList.add("eliminar-btn");
+    div_Tareas.appendChild(deleteButton)
+    
+    //Agregar nuestro nuevo div a la lista de tareas
+    lista_Tareas.appendChild(div_Tareas)
+    // Limpiar valor del input-tareas
+    input_Tareas.value = "";
+}

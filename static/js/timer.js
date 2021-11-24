41bf404a5499d7b0e05f04718c129ec31c46b037
LDAntelo <luisdiegoantelo0@gmail.comgit config --listgit config --listgit config --global user.name LDAntelogit.config --global user.email luisdiegoantelo0@gmail.com>
2021-11-24 16:23:58 -0300

    timer

--- static/js/timer.js
+++ static/js/timer.js
@@ -19,8 +19,8 @@ function start_timer() {
 
   // Change the minutes and seconds to starting time
   session_minutes = 00;
-  session_seconds = 01;
-  session_count = 4;
+  session_seconds = 03;
+  session_count = 0;
   // Add the seconds and minutes to the page
   document.getElementById("minutes").innerHTML = session_minutes;
   document.getElementById("seconds").innerHTML = session_seconds;
@@ -36,7 +36,7 @@ function start_timer() {
     session_count = 1;
     if (session_seconds <= 0) {
       if(session_minutes <= 0) {
-        session_count = session_count - 1;
+        session_count = session_count + 1;
       }
     }
   }
@@ -57,12 +57,11 @@ function start_timer() {
         // Clears the interval i.e. stops the counter
         clearInterval(minutes_interval);
         clearInterval(seconds_interval);
-        session_count = session_count -1;
+        session_count = session_count + 1;
         document.getElementById("sessions").innerHTML = "Sesión: " + session_count;
         // Add the message to the html
         document.getElementById("done").innerHTML =
           "Sesión completada!! Toma un descanso";
-
         // Make the html message div visible
         document.getElementById("done").classList.add("show_message");
         // PLay the bell sound to tell the end of session
@@ -70,6 +69,7 @@ function start_timer() {
       } 
       // Reset the session seconds to 60
       session_seconds = 60;
+      session_minutes = 25;  
     }
   }
 }
